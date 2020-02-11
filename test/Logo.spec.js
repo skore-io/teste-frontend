15a5001cfb6b03a97a6f53a7ecd1df20e49b6ce6
Randy Tellez Galan <kronhyx@gmail.com>
2020-02-11 18:16:03 -0300

    add jest and mocha types

--- test/Logo.spec.js
+++ test/Logo.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import Card from '@/components/Card.vue'
 
 describe('Logo', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
+    const wrapper = mount(Card)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
