1345c37f88ccb694f919f7fc6213bee5dd3acb3c
Randy Tellez Galan <kronhyx@gmail.com>
2020-02-11 18:23:12 -0300

    add first test

--- test/Logo.spec.js
+++ test/Logo.spec.js
@@ -1,9 +1,30 @@
 import { mount } from '@vue/test-utils'
 import Card from '@/components/Card.vue'
 
-describe('Logo', () => {
+describe('Card Component', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Card)
+    const wrapper = mount(Card, {
+      propsData: {
+        data: {
+          id: '15a9LAUXCm9FkAVexH4q',
+          company_id: '114',
+          name: 'Desenvolvedor Backend',
+          thumb_url: 'https://img.skore.io/image/upload/v1566918438/retool/c6mjszo.png',
+          priority: 1,
+          due_date: {
+            available_at: 1572127435329
+          },
+          status: 'IN_PROGRESS',
+          enrollment: {
+            id: '114_15a9LAUXCm9FkAVexH4q_453995',
+            percentage: 21
+          }
+        }
+      }
+    })
     expect(wrapper.isVueInstance()).toBeTruthy()
+    const vm = wrapper.vm
+
+    expect(vm.hasEnrollment).toBeTruthy()
   })
 })
