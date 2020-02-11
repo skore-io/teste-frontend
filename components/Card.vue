9772253a43933c9a5865c051323b55e241e3b82c
Randy Tellez Galan <kronhyx@gmail.com>
2020-02-11 17:45:27 -0300

    add class and icons to cards

--- components/Card.vue
+++ components/Card.vue
@@ -2,12 +2,14 @@
   <b-card bg-variant="light" :img-src="data.thumb_url" body-class="px-2" overlay>
     <b-row class="h-100 text-center" align-content="between" no-gutters>
       <div />
-      <div>{{ data.name }}</div>
+      <div class="card-title">
+        {{ data.name }}
+      </div>
       <div class="w-100">
-        <b-progress v-if="data.enrollment" :value="data.enrollment.percentage" variant="success" show-progress animated />
-        <b-button v-else pill variant="success">
-          {{ data.status }}
-        </b-button>
+        <b-progress v-if="hasEnrollment" :value="data.enrollment.percentage" :variant="statusClass.variant" show-progress animated />
+        <b-badge v-else :variant="statusClass.variant" pill>
+          <i class="fa" :class="statusClass.icon" />
+        </b-badge>
       </div>
     </b-row>
   </b-card>
@@ -43,9 +45,48 @@ export enum Status {
   BLOCKED = 'BLOCKED'
 }
 
+export interface StatusCard {
+  variant: String
+  icon: String
+}
+
 @Component({ name: 'VCard' })
 export default class VCard extends Vue {
   @Prop({ type: Object, required: true }) readonly data!: Card
+
+  get hasEnrollment (): Boolean {
+    return !!this.data.enrollment
+  }
+
+  get statusClass (): StatusCard {
+    switch (this.data.status) {
+      case Status.BLOCKED:
+        return {
+          variant: 'danger',
+          icon: 'fa-lock'
+        }
+      case Status.COMPLETED:
+        return {
+          variant: 'success',
+          icon: 'fa-check'
+        }
+      case Status.IN_PROGRESS:
+        return {
+          variant: 'warning',
+          icon: 'fa-tasks'
+        }
+      case Status.NOT_STARTED:
+        return {
+          variant: 'dark',
+          icon: 'fa-clock-o'
+        }
+      default:
+        return {
+          variant: 'transparent',
+          icon: 'fa-edit'
+        }
+    }
+  }
 }
 </script>
 
@@ -54,5 +95,9 @@ export default class VCard extends Vue {
   main{
     min-height: 150px;
     background-color: palevioletred;
+  }
+
+  .card-title{
+    font-size: 120%;
   }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,8 @@ const config: Configuration = {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' }
     ]
   },
 
