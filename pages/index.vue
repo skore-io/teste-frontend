93f298d153a8c32c1a970bc8c42d1a6f8b46bdc7
Randy Tellez Galan <kronhyx@gmail.com>
2020-02-11 16:07:34 -0300

    add index.d file

--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,9 @@
   <b-container>
     <div>
       <logo />
-      <h1 class="title">
-        teste-frontend
-      </h1>
-      <h2 class="subtitle">
-        My mind-blowing Nuxt.js project
-      </h2>
-      <div class="links">
+      <h1>teste-frontend</h1>
+      <h2>My mind-blowing Nuxt.js project</h2>
+      <div>
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
@@ -30,27 +26,3 @@ export default class IndexPage extends Vue {
   }
 }
 </script>
-
-<style>
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
