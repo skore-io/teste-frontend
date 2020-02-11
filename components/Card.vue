aa37c77a1e92fae0f9a9797226779c7dbf72758d
Randy Tellez Galan <kronhyx@gmail.com>
2020-02-11 17:18:54 -0300

    add progress to card

--- components/Card.vue
+++ components/Card.vue
@@ -1,10 +1,15 @@
 <template>
-  <b-card bg-variant="info" :img-src="data.thumb_url" tag="article" overlay>
-    <b-card-text>{{ data.name }}</b-card-text>
-
-    <b-button href="#" variant="primary">
-      Go somewhere
-    </b-button>
+  <b-card bg-variant="light" :img-src="data.thumb_url" body-class="px-2" overlay>
+    <b-row class="h-100 text-center" align-content="between" no-gutters>
+      <div />
+      <div>{{ data.name }}</div>
+      <div class="w-100">
+        <b-progress v-if="data.enrollment" :value="data.enrollment.percentage" variant="success" show-progress animated />
+        <b-button v-else pill variant="success">
+          {{ data.status }}
+        </b-button>
+      </div>
+    </b-row>
   </b-card>
 </template>
 
@@ -12,31 +17,31 @@
 import { Component, Vue, Prop } from 'nuxt-property-decorator'
 
 export interface Card {
-    id: String
-    company_id: String
-    name: String
-    thumb_url: String | null
-    priority: Number,
-    due_date: DueDate | null
-    status: String
-    enrollment?: Enrollment
-  }
+  id: String
+  company_id: String
+  name: String
+  thumb_url: String | null
+  priority: Number,
+  due_date: DueDate | null
+  status: String
+  enrollment?: Enrollment
+}
 
 export interface DueDate {
-    available_at: Number
-  }
+  available_at: Number
+}
 
 export interface Enrollment {
-    id: String
-    percentage: Number
-  }
+  id: String
+  percentage: Number
+}
 
 export enum Status {
-    IN_PROGRESS = 'IN_PROGRESS',
-    NOT_STARTED = 'NOT_STARTED',
-    COMPLETED = 'COMPLETED',
-    BLOCKED = 'BLOCKED'
-  }
+  IN_PROGRESS = 'IN_PROGRESS',
+  NOT_STARTED = 'NOT_STARTED',
+  COMPLETED = 'COMPLETED',
+  BLOCKED = 'BLOCKED'
+}
 
 @Component({ name: 'VCard' })
 export default class VCard extends Vue {
