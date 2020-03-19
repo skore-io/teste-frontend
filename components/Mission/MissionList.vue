43d4469bdeedeaa58fa164300c66f237b5553c48
Iago <yagomello16@gmail.com>
2020-03-19 16:30:44 -0300

    Added MissionList comonent

---
+++ components/Mission/MissionList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="mission-list">
+        <div v-for="mission in missions" :key="mission.id">
+            <mission-card :mission="mission" />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropOptions } from 'vue'
+import MissionCard from './MissionCard.vue'
+import { IMission } from '~/models/mission'
+export default Vue.extend({
+    name: 'MissionList',
+    components: {
+        MissionCard,
+    },
+    props: {
+        missions: {
+            type: Array,
+            required: true
+        } as PropOptions<IMission[]>,
+    },
+})
+</script>
+
+<style scoped>
+</style>
