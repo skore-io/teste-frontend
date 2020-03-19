99ff00e35a61b3ba68c7089c64e759e9192a169b
Iago <yagomello16@gmail.com>
2020-03-19 16:30:57 -0300

    Added Missions Page

---
+++ pages/missions/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="missions-page">
+        <h1>Missions List</h1>
+        <mission-list :missions="missions" />
+    </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import MissionList from '~/components/Mission/MissionList.vue'
+export default Vue.extend({
+    name: 'MissionsPage',
+    components: {
+        MissionList,
+    },
+    computed: {
+        missions() {
+            return this.$store.state.missions.missions
+        }
+    }
+})
+</script>
+
+<style scoped>
+</style>
