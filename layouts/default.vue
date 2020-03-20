8efeb8d66b1082ee1a5ff0e03846aa63b05f316b
Iago <yagomello16@gmail.com>
2020-03-20 14:38:22 -0300

    layouts/default: added navbar | main container | title logic

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,57 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+    <div class="default-layout">
+        <the-navbar />
+        <main-container :title="pageTitle">
+            <nuxt />
+        </main-container>
+    </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue'
+import TheNavbar from '~/components/Navigation/TheNavbar.vue'
+export default Vue.extend({
+    name: 'DefaultLayout',
+    components: {
+        TheNavbar,
+    },
+    computed: {
+        pageTitle() {
+            return this.$store.state.containerTitle 
+        }
+    },
+})
+</script>
+
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
 .button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+    display: inline-block;
+    border-radius: 4px;
+    border: 1px solid #3b8070;
+    color: #3b8070;
+    text-decoration: none;
+    padding: 10px 30px;
+    cursor: pointer;
 }
 
 .button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+    color: #fff;
+    background-color: #3b8070;
 }
 
 .button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
+    display: inline-block;
+    border-radius: 4px;
+    border: 1px solid #35495e;
+    color: #35495e;
+    text-decoration: none;
+    padding: 10px 30px;
+    margin-left: 15px;
+    cursor: pointer;
 }
 
 .button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+    color: #fff;
+    background-color: #35495e;
 }
 </style>
