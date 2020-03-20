486e87abbf785c91ddc7ba1f5c4ac7dd4705a0d2
Iago <yagomello16@gmail.com>
2020-03-20 14:35:23 -0300

    store/index: added page config data

--- store/index.ts
+++ store/index.ts
@@ -1,6 +1,18 @@
-import { ActionTree } from 'vuex'
+import { ActionTree, MutationTree } from 'vuex'
 
-export const actions: ActionTree<any, any> = {
+export const state = () => ({
+    containerTitle: ''
+})
+
+export type RootState = ReturnType<typeof state>
+
+export const mutations: MutationTree<RootState> = {
+    setContainerTitle(state, title) {
+        state.containerTitle = title
+    }
+}
+
+export const actions: ActionTree<RootState, RootState> = {
     async nuxtServerInit({dispatch}) {
         await dispatch('missions/fetchMissions')
     }
