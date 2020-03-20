a376b9d233d6040fe58440008511bd6c8d0d2ce0
Iago <yagomello16@gmail.com>
2020-03-20 14:35:55 -0300

    types/status: changed Status to enum | added StatusColor enum

--- types/status.ts
+++ types/status.ts
@@ -1,2 +1,14 @@
-type Status = 'NOT_STARTED' | 'IN_PROGRESS' | 'COMPLETED' | 'FAILED' | 'BLOCKED'
-export { Status }
+export enum Status {
+    'NOT_STARTED' = 'NOT STARTED',
+    'IN_PROGRESS' = 'IN PROGRESS',
+    'COMPLETED' = 'COMPLETED',
+    'FAILED' = 'FAILED',
+    'BLOCKED' = 'BLOCKED'
+}
+export enum StatusColor {
+    'NOT_STARTED' = '#a8a8a8',
+    'IN_PROGRESS' = '#6b88af',
+    'COMPLETED' = '#7cd992',
+    'FAILED' = '#f7e463',
+    'BLOCKED' = '#eb6060'
+}
