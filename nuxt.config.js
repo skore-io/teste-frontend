8790a58efeb1cd695bc16a9ab34d892b02bb7bb4
Iago <yagomello16@gmail.com>
2020-03-20 14:31:58 -0300

    nuxt.config: adding fonts | plugins | scss

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,23 +12,30 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href:"https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" },
     ]
   },
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: { color: '#5ac2a9', height: '4px' },
   /*
   ** Global CSS
   */
   css: [
+    '~assets/styles/main.scss'
   ],
+  styleResources: {
+    scss: []
+  },
   /*
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '~/plugins/axios-accessor.ts'
+    '~/plugins/core-components.ts',
+    '~/plugins/axios-accessor.ts',
+    '~/plugins/date-filter.ts'
   ],
   /*
   ** Nuxt.js dev-modules
@@ -42,13 +49,15 @@ export default {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
+    '@nuxtjs/style-resources'
   ],
   /*
   ** Axios module configuration
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    baseURL: 'https://us-central1-teste-frontend-c2dcc.cloudfunctions.net' || process.env.baseUrl
+    baseURL: 'https://us-central1-teste-frontend-c2dcc.cloudfunctions.net' || process.env.baseUrl,
+    credentials: false
   },
   /*
   ** Build configuration
@@ -58,5 +67,12 @@ export default {
     ** You can extend webpack config here
     */
     extend (/*config, ctx*/) { }
+  },
+  router: {
+    linkExactActiveClass: 'active'
+  },
+  pageTransition: {
+    name: 'fade',
+    mode: 'out-in'
   }
 }
