9528351e20edad6de3ce8958be46309149f28aec
Iago <yagomello16@gmail.com>
2020-03-20 14:39:26 -0300

    Finished pages/missions/index

--- pages/missions/index.vue
+++ pages/missions/index.vue
@@ -1,25 +1,42 @@
 <template>
-    <section class="missions-page">
-        <h1>Missions List</h1>
-        <mission-list :missions="missions" />
-    </section>
+    <div class="missions-page">
+        <mission-card
+            v-for="mission in missions"
+            :key="mission.id"
+            :mission="mission"
+            @click="handleMissionTransition"
+        />
+    </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import MissionList from '~/components/Mission/MissionList.vue'
+import MissionCard from '~/components/Mission/MissionCard.vue'
 export default Vue.extend({
     name: 'MissionsPage',
     components: {
-        MissionList,
+        MissionCard,
+    },
+    created () {
+        this.$store.commit('setContainerTitle', 'SAROK MISSIONS')
     },
     computed: {
         missions() {
             return this.$store.state.missions.missions
         }
-    }
+    },
+    methods: {
+        handleMissionTransition(missionId) {
+            this.$router.push(`/missions/${missionId}`)
+        }
+    },
 })
 </script>
 
 <style scoped>
+.missions-page {
+    display: grid;
+    gap: 1.5em;
+    grid-template-columns: repeat(2, 1fr);
+}
 </style>
