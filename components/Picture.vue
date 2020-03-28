328be052a0975683fe48660e053e928578e2df9d
Guilherme Barbosa <guui.wb@gmail.com>
2020-03-28 19:47:10 -0300

    Creating picture component

---
+++ components/Picture.vue
@@ -0,0 +1,27 @@
+<template>
+  <img :src="url" alt />
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true
+    },
+
+    alt: {
+      type: String,
+      default: "picture"
+    }
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+img
+  width: 100px
+  height: 100px
+  box-shadow: 10px 10px 15px -8px rgba(0,0,0,0.46)
+  border-radius: 100%
+</style>
