740103313efd944bb29f6f486ca70939c306acfd
Guilherme Barbosa <guui.wb@gmail.com>
2020-03-28 20:53:19 -0300

    Showing 10 mission cards on index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,54 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        teste-frontend
-      </h1>
-      <h2 class="subtitle">
-        Teste frontend
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <main>
+    <div class="container">
+      <card v-for="n in 10" :key="n" :mission="mission" />
     </div>
-  </div>
+  </main>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Card from "~/components/MissionCard/Main.vue";
 
 export default {
+  data() {
+    return {
+      mission: {
+        id: "15a9LAUXCm9FkAVexH4q",
+        company_id: "114",
+        name: "Desenvolvedor Backend",
+        thumb_url:
+          "https://img.skore.io/image/upload/v1566918438/retool/c6mjszo.png",
+        priority: 1,
+        due_date: {
+          available_at: 1572127435329
+        },
+        status: "IN_PROGRESS",
+        enrollment: {
+          id: "114_15a9LAUXCm9FkAVexH4q_453995",
+          percentage: 21
+        }
+      }
+    };
+  },
   components: {
-    Logo
+    Card
   }
-}
+};
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="sass" scoped>
+main
+  font-family: 'Source Sans Pro', sans-serif
+  background-color: rgb(17, 17, 22)
+  min-height: 100vh
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+  .container
+    display: flex
+    flex-direction: row
+    flex-wrap: wrap
+    justify-content: space-between
+    justify-self: center
+    align-items: flex-start
+    width: 980px
+    margin: 0 auto
 </style>
