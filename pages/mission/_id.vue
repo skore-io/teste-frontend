0fbd05792e54c3fa0aae62921b9c3e41fd81456b
Guilherme Barbosa <guui.wb@gmail.com>
2020-03-29 15:46:46 -0300

    Fetching mission

--- pages/mission/_id.vue
+++ pages/mission/_id.vue
@@ -4,10 +4,32 @@
 
 <script>
 export default {
+  data() {
+    return {
+      mission: null
+    };
+  },
   computed: {
     id() {
       return this.$route.params.id;
     }
+  },
+  methods: {
+    async getMission() {
+      try {
+        let res = await fetch(
+          `https://us-central1-teste-frontend-c2dcc.cloudfunctions.net/missions/${this.id}`
+        );
+        if (res.status === 200) this.mission = await res.json();
+        else if (res.status === 404) throw "Missão inexistente!";
+        else throw "Ocorreu um erro inesperado!";
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  },
+  mounted() {
+    this.getMission();
   }
 };
 </script>
