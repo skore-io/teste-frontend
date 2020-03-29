10ce57be038065365c284dd365212a85583845d4
Guilherme Barbosa <guui.wb@gmail.com>
2020-03-29 20:11:40 -0300

    Displaying error with auto redirect

---
+++ components/Error.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="error">
+    <img src="/svg/cross.svg" alt="erro" />
+    <p>{{message}}</p>
+    <span>Você será redirecionado em {{seconds}}s</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      seconds: 10
+    };
+  },
+  props: {
+    message: String
+  },
+  mounted() {
+    setInterval(() => {
+      if (this.seconds > 0) this.seconds--;
+    }, 1000);
+  },
+  watch: {
+    seconds() {
+      if (!this.seconds) this.$router.push("/");
+    }
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+.error
+  width: 100%
+  height: 100vh
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+  text-align: center
+  padding: 0 45px
+
+  img
+    height: 100px
+
+  p
+    font-size: 20px
+</style>
--- pages/mission/_id.vue
+++ pages/mission/_id.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="detail">
     <img v-if="loading" src="/svg/loader.svg" height="50px" alt="carregando..." />
+    <error v-else-if="error" :message="error" />
     <card-detail v-else :mission="mission" />
   </div>
 </template>
 
 <script>
+import Error from "~/components/Error";
 import CardDetail from "~/components/missions/Detail";
 
 export default {
   components: {
+    Error,
     CardDetail
   },
   data() {
     return {
       loading: false,
-      mission: null
+      mission: null,
+      error: ""
     };
   },
   computed: {
@@ -32,10 +36,11 @@ export default {
           `https://us-central1-teste-frontend-c2dcc.cloudfunctions.net/missions/${this.id}`
         );
         if (res.status === 200) this.mission = await res.json();
-        else if (res.status === 404) throw "Missão inexistente!";
-        else throw "Ocorreu um erro inesperado!";
+        else if (res.status === 404)
+          throw "Ah não! Você não pode acessar essa missão ou ela não existe ;(";
+        else throw "Ocorreu um erro inesperado, tente novamente mais tarde!";
       } catch (e) {
-        console.log(e);
+        this.error = e;
       }
 
       this.loading = false;
