3574e630029d25e309310877dcee20e934f447ad
Guilherme Barbosa <guui.wb@gmail.com>
2020-03-28 21:56:55 -0300

    Creating status component

--- components/MissionCard/Status.vue
+++ components/MissionCard/Status.vue
@@ -1,10 +1,54 @@
 <template>
-  <div></div>
+  <div class="status">
+    <div class="circle" :style="`background-color: ${color}`"></div>
+    {{name}}
+  </div>
 </template>
 
 <script>
-export default {};
+const STATUS_MAP = {
+  COMPLETED: {
+    color: "#29ad5b",
+    name: "concluído"
+  },
+  IN_PROGRESS: {
+    color: "#1077f7",
+    name: "em andamento"
+  },
+  NOT_STARTED: {
+    color: "#343434",
+    name: "não iniciado"
+  },
+  BLOCKED: {
+    color: "#de1a1a",
+    name: "bloqueado"
+  }
+};
+
+export default {
+  props: {
+    status: String
+  },
+  computed: {
+    color() {
+      return STATUS_MAP[this.status].color;
+    },
+    name() {
+      return STATUS_MAP[this.status].name;
+    }
+  }
+};
 </script>
 
 <style lang="sass" scoped>
+.status
+  display: flex
+  align-items: center
+  text-transform: capitalize
+
+  .circle
+    width: 20px
+    height: 20px
+    border-radius: 100%
+    margin-right: 5px
 </style>
