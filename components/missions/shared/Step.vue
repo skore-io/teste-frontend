1d77b1a8946aac7d3e2d149741abcf8088edd0ec
Guilherme Barbosa <guui.wb@gmail.com>
2020-03-29 17:15:41 -0300

    Creating step component

--- components/missions/shared/Step.vue
+++ components/missions/shared/Step.vue
@@ -1,7 +1,44 @@
 <template>
-  <div></div>
+  <div class="step">
+    <div class="circle">{{ step.id }}</div>
+    <div>
+      <h2>{{step.name}}</h2>
+      <v-status :status="step.status || 'NOT_STARTED'" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import vStatus from "./Status";
+
+export default {
+  props: {
+    step: Object
+  },
+  components: {
+    vStatus
+  }
+};
 </script>
+
+<style lang="sass" scoped>
+.step
+  display: flex
+  margin: 20px 0
+
+  .circle
+    display: flex
+    justify-content: center
+    align-items: center
+    width: 80px
+    height: 80px
+    border-radius: 100%
+    background: rgba(white, .03)
+    border: 2px solid rgba(white, .06)
+    font-size: 40px
+    font-weight: 700
+    margin-right: 20px
+
+  h2
+    margin-bottom: 5px
+</style>
