4c1634f131206461088b38feec435b82c83813bc
Guilherme Barbosa <guui.wb@gmail.com>
2020-03-29 23:20:29 -0300

    Creating vuex store

---
+++ store/index.js
@@ -0,0 +1,64 @@
+export const state = () => ({
+  missions: null,
+  mission: null,
+  filter: "all",
+  loader: false,
+  error: null
+});
+
+export const getters = {
+  list(state) {
+    return state.filter === "all"
+      ? state.missions
+      : state.missions.filter(el => el.status === state.filter);
+  }
+};
+
+export const actions = {
+  async fetchAll({ commit }) {
+    try {
+      commit("setLoader", true);
+      let res = await this.$axios.$get("/missions");
+      commit("setMissions", res);
+      commit("setLoader", false);
+    } catch (e) {
+      commit("setLoader", false);
+      throw e;
+    }
+  },
+  async getMission({ commit }, id) {
+    try {
+      let res = await this.$axios.$get(`/missions/${id}`);
+      commit("setMission", res);
+    } catch (e) {
+      e.status === 404
+        ? commit(
+            "setError",
+            "Ah não! Você não pode acessar essa missão ou ela não existe ;("
+          )
+        : commit(
+            "setError",
+            "Ocorreu um erro inesperado, tente novamente mais tarde!"
+          );
+      throw e;
+    }
+  }
+};
+
+export const mutations = {
+  setMissions(state, payload) {
+    state.missions = payload;
+  },
+  setMission(state, payload) {
+    state.mission = payload;
+  },
+  setFilter(state, payload) {
+    state.filter = payload;
+  },
+  setLoader(state, payload) {
+    state.loader = payload;
+  },
+  setError(state, payload) {
+    state.error = payload;
+  }
+};
