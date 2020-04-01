cb8994ce9aa349984a2555332f1e5e25f82c7ed2
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-04-01 10:24:46 -0300

    Extrair component para cards de Missions

---
+++ components/MissionArticle.vue
@@ -0,0 +1,38 @@
+<template>
+  <article v-if="mission" @click="pushDetailedMission(mission.id)">
+    <h3>{{mission.name}}</h3>
+    <img :src="mission.thumb_url" alt="Thumbnail da Mission" />
+    <!-- <nuxt-link :to="{ name: 'missions-id', params: { id: mission.id}}">Detalhar!</nuxt-link> -->
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    mission: {
+      required: true,
+      type: Object
+    }
+  },
+  methods: {
+    pushDetailedMission(missionId) {
+      this.$router.push({ name: "missions-id", params: { id: missionId } });
+    }
+  }
+};
+</script>
+
+<style scoped>
+article {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+  border: 1px solid grey;
+  padding: 1rem;
+}
+
+img {
+  height: 150px;
+}
+</style>
