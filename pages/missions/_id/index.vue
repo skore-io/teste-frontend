915f950d51a827e26d9988be810c7154219448d9
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-31 21:01:46 -0300

    Exibir informações da missão selecionada

--- pages/missions/_id/index.vue
+++ pages/missions/_id/index.vue
@@ -1,8 +1,26 @@
 <template>
-  <div>
+  <section>
     <nuxt-link :to="{ name: 'index'}">Voltar</nuxt-link>
-    <p>{{selectedMission}}</p>
-  </div>
+    <template v-if="selectedMission">
+      <div>
+        <h3>{{selectedMission.name}}</h3>
+        <p>
+          <strong>Status: {{selectedMission.status}}</strong>
+        </p>
+        <img :src="selectedMission.thumb_url" alt="Thumbnail da Mission" />
+        <article>
+          <h4>Visão do Curso</h4>
+          <ul>
+            <li v-for="step in selectedMission.steps" :key="step.id">{{step.name}}</li>
+          </ul>
+        </article>
+        <article v-if="enrollment">
+          <h4>Sua inscrição</h4>
+          <meter min="0" max="100" :value="enrollment.percentage"></meter>
+        </article>
+      </div>
+    </template>
+  </section>
 </template>
 
 <script>
@@ -15,13 +33,16 @@ export default {
     };
   },
   methods: {
-    ...mapActions(['fetchDetailed'])
+    ...mapActions(["fetchDetailed"])
   },
   computed: {
-    ...mapState(['selectedMission'])
+    ...mapState(["selectedMission"]),
+    enrollment() {
+      return this.selectedMission.enrollment;
+    }
   },
   async mounted() {
-    await this.fetchDetailed(this.id)
+    await this.fetchDetailed(this.id);
   }
 };
 </script>
