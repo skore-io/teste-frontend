ad2a94d3769d5a383407d54c7fb149353165f237
Johann Borges <johannborges@hotmail.com>
2020-10-10 13:00:58 -0300

    listagem de missoes estilizada

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,52 +6,25 @@
 
 <style>
 @import url("https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css");
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap");
 
 html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
-    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: Roboto, sans-serif;
   font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+
+  background: #eee;
 }
 
 *,
 *:before,
 *:after {
   box-sizing: border-box;
-  margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.container {
+  max-width: 100%;
+  width: 1300px;
+  margin: auto;
+  padding: 0 15px;
 }
 </style>
--- pages/missions/index.vue
+++ pages/missions/index.vue
@@ -1,28 +1,45 @@
 <template>
-  <p v-if="missions.length < 1">Fetching posts...</p>
+  <div class="container">
+    <div class="missions">
+      <p v-if="missions.length < 1">Fetching missions...</p>
 
-  <div v-else>
-    <select v-model="filter">
-      <option value="">Todos</option>
+      <div v-else>
+        <div class="missions-header">
+          <div class="missions-filter">
+            <div>Filtrar missões por status:</div>
 
-      <option
-        v-for="(statusItem, statusKey, statusIndex) of status"
-        :key="`${statusKey}-${statusIndex}`"
-        :value="statusKey"
-        >{{ statusItem }}
-      </option>
-    </select>
+            <select v-model="filter">
+              <option value="">Todos</option>
 
-    <ul>
-      <li
-        v-for="(missionItem, missionIndex) of filterMissions()"
-        :key="`${missionIndex}-${missionItem.id}`"
-      >
-        <nuxt-link :to="`/missions/${missionItem.id}`"
-          >{{ missionItem.name }} - {{ status[missionItem.status] }}</nuxt-link
-        >
-      </li>
-    </ul>
+              <option
+                v-for="(statusItem, statusKey, statusIndex) of status"
+                :key="`${statusKey}-${statusIndex}`"
+                :value="statusKey"
+                >{{ statusItem }}
+              </option>
+            </select>
+          </div>
+
+          <div class="missions-list-header">
+            <div>Missão</div>
+            <div>Status</div>
+          </div>
+        </div>
+
+        <ul class="missions-list">
+          <li
+            v-for="(missionItem, missionIndex) of filterMissions()"
+            :key="`${missionIndex}-${missionItem.id}`"
+            class="mission-item"
+          >
+            <nuxt-link :to="`/missions/${missionItem.id}`">{{
+              missionItem.name
+            }}</nuxt-link>
+            <div>{{ status[missionItem.status] }}</div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -67,4 +84,62 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.missions {
+  padding-bottom: 30px;
+}
+
+.missions .missions-header {
+  position: sticky;
+  top: 0;
+  z-index: 2;
+
+  padding: 30px 0 15px;
+
+  background: #eee;
+  border-bottom: 1px solid #ddd;
+}
+
+.missions .missions-header > .missions-filter {
+  display: flex;
+  align-items: center;
+}
+
+.missions .missions-header > .missions-filter > div {
+  padding-right: 10px;
+}
+
+.missions .missions-header > .missions-filter > select {
+  padding: 10px;
+
+  border: 1px solid #999;
+}
+
+.missions .missions-header > .missions-list-header {
+  display: flex;
+  justify-content: space-between;
+
+  padding-top: 15px;
+
+  text-transform: uppercase;
+  font-weight: 500;
+}
+
+.missions .missions-list > .mission-item {
+  display: flex;
+  justify-content: space-between;
+
+  padding-top: 15px;
+
+  font-weight: 300;
+  font-size: 14px;
+}
+
+.missions .missions-list > .mission-item > a {
+  text-decoration: none;
+}
+
+.missions .missions-list > .mission-item > a:hover {
+  text-decoration: underline;
+}
+</style>
