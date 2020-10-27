2fa3ef7bc32e93427f721d58687b8c19edb73d08
luquinha <lucaspirlo55@gmail.com>
2020-10-27 16:14:26 -0300

    [Feat]: Add card component to render mission infos

---
+++ components/Card.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+<style>
+.card {
+  background: white;
+  border-radius: 20px;
+  -webkit-box-shadow: 5px 5px 15px 5px #000000;
+  box-shadow: 5px 5px 15px 5px #000000;
+}
+</style>
