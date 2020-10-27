59bcfadbf016d027b4b4276f979e11814a8b4fb5
luquinha <lucaspirlo55@gmail.com>
2020-10-27 16:15:10 -0300

    [Feat]: add components to main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,22 @@
 <template>
   <div class="container">
     <div>
-      <logo />
       <h1 class="title">
-        teste-frontend
+        Missões
       </h1>
-      <h2 class="subtitle">
-        Teste frontend
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <Missions></Missions>
     </div>
   </div>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
+import Missions from '~/components/Missions.vue'
 
 export default {
   components: {
-    Logo
+    Logo,
+    Missions
   }
 }
 </script>
