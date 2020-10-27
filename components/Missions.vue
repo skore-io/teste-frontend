bce008ce58b6ec1c179dfe89795a132aab252901
luquinha <lucaspirlo55@gmail.com>
2020-10-27 16:56:46 -0300

    [Feat]: add search field to status

--- components/Missions.vue
+++ components/Missions.vue
@@ -1,8 +1,22 @@
 <template>
-  <div>
+  <div class="missions">
+    <div class="search">
+      <Select
+        label="Status"
+        :options="[
+          'NOT_STARTED',
+          'IN_PROGRESS',
+          'COMPLETED',
+          'BLOCKED'
+          ]"
+          @input="selected = $event"
+        />
+    </div>
+    <button v-on:click="search">Filtrar</button>
+    <button v-on:click="clean">Limpar</button>
     <div class="missions-container">
       <Card
-        v-for="mission in missions"
+        v-for="mission in filteredMissions"
         v-bind:key="mission.id"
       >
         <div class="content">
@@ -20,27 +34,43 @@
 <script>
 import Axios from 'axios'
 import Card from './Card'
-
+import Select from './Select'
 export default {
   components: {
-    Card
+    Card,
+    Select
   },
   data() {
     return {
-      missions: []
+      missions: [],
+      filteredMissions: [],
+      selected: 'IN_PROGRESS'
+    }
+  },
+  methods: {
+    search: function() {
+      this.filteredMissions = this.missions.filter(mission => mission.status === this.selected)
+    },
+    clean: function() {
+      this.filteredMissions = this.missions
     }
   },
   async mounted() {
     const { data } = await Axios.get('https://us-central1-teste-frontend-c2dcc.cloudfunctions.net/missions')
     this.missions = data
+    this.filteredMissions = data
   }
 }
 </script>
 <style scoped>
+.missions {
+  width: 100%;
+}
 .missions-container {
   display: flex;
-  margin: 0 -10px;
+  margin: 0 auto;
   flex-wrap: wrap;
+  max-width: 900px;
   justify-content: flex-start;
 }
 
@@ -51,11 +81,21 @@ export default {
   margin: 30px auto;
 }
 
+.content h2 {
+  font-size: 18px;
+margin-bottom: 20px;
+}
+
 .card {
   padding: 20px;
   margin: 15px;
-  max-width: 21%;
-  flex: 1 0 21%; /* explanation below */
+  max-width: 30%;
+  flex: 1 0 30%; /* explanation below */
+}
+
+.search {
+  max-width: 320px;
+  margin: 20px auto;
 }
 
 .card:last-child {
